f7cdca5fed8118833e5d9a56375022b567c4b606
Tanisha gupta <154671056+Taneesha000@users.noreply.github.com>
2025-09-03 17:54:55 +0530

    Time: 136 ms (51.06%) | Memory: 41.2 MB (67.02%) - LeetSync

---
+++ 3277-find-the-number-of-ways-to-place-people-ii/find-the-number-of-ways-to-place-people-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    static bool comp(vector<int>& a, vector<int>& b) {
+        if (a[0] == b[0])
+            return a[1] > b[1];
+        return a[0] < b[0];
+    }
+
+public:
+    int numberOfPairs(vector<vector<int>>& points) {
+        int n = points.size();
+        sort(points.begin(), points.end(), comp);
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int y_mn=INT_MAX;
+            for (int j = i - 1; j >= 0; j--) {
+
+                if (points[j][1] >= points[i][1]) {
+                  if(y_mn>points[j][1]){
+                    y_mn=points[j][1];
+                    ans++;
+                  }
+                }
+            }
+           
+        }
+        return ans;
+    }
+};
+
+
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
